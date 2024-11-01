4d607d49e120522327cdf1c37691a431395cb161
James Cherti <60946298+jamescherti@users.noreply.github.com>
2024-11-01 09:59:17 -0400

    Rename the project to quick-sdcv

--- README.md
+++ README.md
@@ -1,22 +1,22 @@
-# easysdcv.el - Emacs interface for the 'sdcv' command-line dictionary
+# quick-sdcv.el - Emacs interface for the 'sdcv' command-line dictionary
 
-The `easysdcv` package serves as an Emacs interface for the `sdcv` command-line interface, which is the console version of the StarDict dictionary application.
+The `quick-sdcv` package serves as an Emacs interface for the `sdcv` command-line interface, which is the console version of the StarDict dictionary application.
 
 This integration allows users to access and utilize dictionary functionalities directly within the Emacs environment, leveraging the capabilities of `sdcv` to look up words and translations from various dictionary files formatted for StarDict.
 
 <!-- markdown-toc start - Don't edit this section. Run M-x markdown-toc-refresh-toc -->
 **Table of Contents**
 
-- [easysdcv.el - Emacs interface for the 'sdcv' command-line dictionary](#easysdcvel---emacs-interface-for-the-sdcv-command-line-dictionary)
+- [quick-sdcv.el - Emacs interface for the 'sdcv' command-line dictionary](#quick-sdcvel---emacs-interface-for-the-sdcv-command-line-dictionary)
     - [Installation](#installation)
         - [1. Install sdcv](#1-install-sdcv)
             - [Linux (Debian/Ubuntu-based operating systems)](#linux-debianubuntu-based-operating-systems)
             - [macOS](#macos)
-        - [2. Require easysdcv.el](#2-require-easysdcvel)
+        - [2. Require quick-sdcv.el](#2-require-quick-sdcvel)
     - [Configuration](#configuration)
     - [Usage](#usage)
     - [Frequently asked question](#frequently-asked-question)
-        - [What is the difference between sdcv (MELPA) and easysdcv Emacs packages?](#what-is-the-difference-between-sdcv-melpa-and-easysdcv-emacs-packages)
+        - [What is the difference between sdcv (MELPA) and quick-sdcv Emacs packages?](#what-is-the-difference-between-sdcv-melpa-and-quick-sdcv-emacs-packages)
     - [Links](#links)
 
 <!-- markdown-toc end -->
@@ -37,23 +37,23 @@ sudo apt-get install sdcv
 brew install sdcv
 ```
 
-### 2. Require easysdcv.el
+### 2. Require quick-sdcv.el
 
-Place `easysdcv.el` in your load-path.
+Place `quick-sdcv.el` in your load-path.
 
 Then, add the following line to your `.emacs` startup file:
 
 ```elisp
-(require 'easysdcv)
+(require 'quick-sdcv)
 ```
 
 ## Configuration
 
 ```elisp
-(setq easysdcv-dictionary-data-dir "startdict_dictionary_directory") ; Set up the directory for the Stardict dictionary
+(setq quick-sdcv-dictionary-data-dir "startdict_dictionary_directory") ; Set up the directory for the Stardict dictionary
 
 ;; Set up the dictionary list for complete search
-(setq easysdcv-dictionary-complete-list
+(setq quick-sdcv-dictionary-complete-list
       '("ENG-FRA Dictionary"
         "FRA-ENG Dictionary"
         "stardict 1.3 ENG-FRA Dictionary"
@@ -61,7 +61,7 @@ Then, add the following line to your `.emacs` startup file:
         "Jargon"))
 ```
 
-After completing the above configuration, execute the `easysdcv-check` Emacs command to confirm that the dictionary settings are correct. Otherwise, `easysdcv` will not function correctly due to the absence of dictionary files in `easysdcv-dictionary-data-dir`.
+After completing the above configuration, execute the `quick-sdcv-check` Emacs command to confirm that the dictionary settings are correct. Otherwise, `quick-sdcv` will not function correctly due to the absence of dictionary files in `quick-sdcv-dictionary-data-dir`.
 
 ## Usage
 
@@ -69,28 +69,28 @@ Below are the commands you can use:
 
 | Command                   | Description
 | :---                      | :---
-| `easysdcv-search-pointer` | Searches the word around the cursor and displays the result in a buffer.
-| `easysdcv-search-input`   | Searches the input word and displays the result in a buffer.
+| `quick-sdcv-search-pointer` | Searches the word around the cursor and displays the result in a buffer.
+| `quick-sdcv-search-input`   | Searches the input word and displays the result in a buffer.
 
-If the current mark is active, the `easysdcv` will translate the region string; otherwise, they will translate the word around the cursor.
+If the current mark is active, the `quick-sdcv` will translate the region string; otherwise, they will translate the word around the cursor.
 
 ## Frequently asked question
 
-### What is the difference between sdcv (MELPA) and easysdcv Emacs packages?
+### What is the difference between sdcv (MELPA) and quick-sdcv Emacs packages?
 
-The `easysdcv` Emacs package is a fork of `sdcv.el` version 3.4, which is available on MELPA. The primary differences between the two packages are as follows:
+The `quick-sdcv` Emacs package is a fork of `sdcv.el` version 3.4, which is available on MELPA. The primary differences between the two packages are as follows:
 
-- **Improved Outline Minor Mode**: The `easysdcv` package fixes the outline minor mode for dictionary folding, enabling users to collapse all definitions for quicker navigation through dictionaries.
+- **Improved Outline Minor Mode**: The `quick-sdcv` package fixes the outline minor mode for dictionary folding, enabling users to collapse all definitions for quicker navigation through dictionaries.
 - **Default Language Settings**: Various issues have been addressed, including changing the default language setting from Chinese (zh) to nil, providing a more neutral starting point.
-- **Buffer Customization**: The `easysdcv` package employs `display-buffer`, allowing users to customize the display of the *SDCV* buffer and control its placement through `display-buffer-alist`.
-- **Removal of bugs and Warnings**: All Emacs warnings have been eliminated and bugs fixed. (e.g., when `easysdcv-search-pointer` cannot locate the word under the cursor)
-- **Code Simplification**: The code has been simplified by removing unused variables and omitting features like posframe, text-to-speech using the 'say' command, the easysdcv-env-lang variable, and functions such as (easysdcv-scroll-up-one-line, easysdcv-scroll-down-one-line, easysdcv-next-line and easysdcv-prev-line) which are similar Emacs features. This simplification makes `easysdcv` easier to understand, maintain, and use by focusing solely on dictionary lookup functionality. Features like `posframe` and text-to-speech, which are not essential to core usage, are better suited as separate packages.
-- **Keybindings removal**: The default keybindings have been removed from `easysdcv-mode` to enhances customizability, prevents conflicts with other modes, and keeps the mode lightweight and adaptable for users’ preferences.
+- **Buffer Customization**: The `quick-sdcv` package employs `display-buffer`, allowing users to customize the display of the *SDCV* buffer and control its placement through `display-buffer-alist`.
+- **Removal of bugs and Warnings**: All Emacs warnings have been eliminated and bugs fixed. (e.g., when `quick-sdcv-search-pointer` cannot locate the word under the cursor)
+- **Code Simplification**: The code has been simplified by removing unused variables and omitting features like posframe, text-to-speech using the 'say' command, the quick-sdcv-env-lang variable, and functions such as (quick-sdcv-scroll-up-one-line, quick-sdcv-scroll-down-one-line, quick-sdcv-next-line and quick-sdcv-prev-line) which are similar Emacs features. This simplification makes `quick-sdcv` easier to understand, maintain, and use by focusing solely on dictionary lookup functionality. Features like `posframe` and text-to-speech, which are not essential to core usage, are better suited as separate packages.
+- **Keybindings removal**: The default keybindings have been removed from `quick-sdcv-mode` to enhances customizability, prevents conflicts with other modes, and keeps the mode lightweight and adaptable for users’ preferences.
 - **Various improvements**: Implement error handling for cases when the sdcv program is not found.
-- **New interactive functions**: easysdcv-list-dictionaries
-- **New defcustom**: easysdcv-exact-search
+- **New interactive functions**: quick-sdcv-list-dictionaries
+- **New defcustom**: quick-sdcv-exact-search
 
 ## Links
 
 - You can download sdcv dictionnaries from http://download.huzheng.org/dict.org/
-- The easysdcv.el Emacs package @GitHub: https://github.com/jamescherti/easysdcv.el
+- The quick-sdcv.el Emacs package @GitHub: https://github.com/jamescherti/quick-sdcv.el
--- easysdcv.el
+++ easysdcv.el
@@ -1,16 +1,16 @@
-;;; easysdcv.el --- Interface for the sdcv command (StartDict cli dictionary) -*- lexical-binding: t -*-
+;;; quick-sdcv.el --- Interface for the sdcv command (StartDict cli dictionary) -*- lexical-binding: t -*-
 
 ;; Copyright (C) 2024 James Cherti | https://www.jamescherti.com/contact/
 ;; Copyright (C) 2009 Andy Stewart <lazycat.manatee@gmail.com>
 
-;; Filename: easysdcv.el
+;; Filename: quick-sdcv.el
 ;; Description: Interface for sdcv (StartDict console version).
 ;; Package-Requires: ((emacs "25.1"))
 ;; Maintainer: James Cherti
 ;; Original Author: Andy Stewart
 ;; Created: 2009-02-05 22:04:02
 ;; Version: 3.6
-;; URL: https://github.com/jamescherti/easysdcv.el
+;; URL: https://github.com/jamescherti/quick-sdcv.el
 ;; Keywords: docs, startdict, sdcv
 
 ;;; This file is NOT part of GNU Emacs
@@ -34,7 +34,7 @@
 
 ;;; Commentary:
 ;;
-;; The `easysdcv` package serves as an Emacs interface for the `sdcv`
+;; The `quick-sdcv` package serves as an Emacs interface for the `sdcv`
 ;; command-line interface, which is the console version of the StarDict
 ;; dictionary application.
 ;;
@@ -44,9 +44,9 @@
 ;; dictionary files formatted for StarDict.
 ;;
 ;; Below are the commands you can use:
-;; - `easysdcv-search-pointer': Searches the word around the cursor and displays
+;; - `quick-sdcv-search-pointer': Searches the word around the cursor and displays
 ;;   the result in a buffer.
-;; - `easysdcv-search-input': Searches the input word and displays the result in
+;; - `quick-sdcv-search-input': Searches the input word and displays the result in
 ;;   a buffer.
 ;;
 
@@ -61,57 +61,57 @@
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;; Customize ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-(defgroup easysdcv nil
+(defgroup quick-sdcv nil
   "Interface for sdcv (StartDict console version)."
   :group 'edit)
 
-(defcustom easysdcv-buffer-name "*SDCV*"
+(defcustom quick-sdcv-buffer-name "*SDCV*"
   "The name of the sdcv buffer."
   :type 'string
-  :group 'easysdcv)
+  :group 'quick-sdcv)
 
-(defcustom easysdcv-program "sdcv"
+(defcustom quick-sdcv-program "sdcv"
   "Path to sdcv."
   :type 'file
-  :group 'easysdcv)
+  :group 'quick-sdcv)
 
-(defcustom easysdcv-dictionary-complete-list nil
-  "A list of dictionaries used for translation in easysdcv.
+(defcustom quick-sdcv-dictionary-complete-list nil
+  "A list of dictionaries used for translation in quick-sdcv.
 Each entry should specify a dictionary source, allowing multiple dictionaries to
 be utilized in the translation process."
   :type '(repeat string)
-  :group 'easysdcv)
+  :group 'quick-sdcv)
 
-(defcustom easysdcv-dictionary-data-dir nil
+(defcustom quick-sdcv-dictionary-data-dir nil
   "The sdcv data directory.
 By default, sdcv searches for words in /usr/share/startdict/dict/.
 If you customize this value with a local directory, you won't need to copy
 dictionary data to the /usr/share directory every time you finish system
 installation."
   :type '(choice (const :tag "Default" nil) directory)
-  :group 'easysdcv)
+  :group 'quick-sdcv)
 
-(defcustom easysdcv-only-data-dir t
-  "Only use the dictionaries in data-dir `easysdcv-dictionary-data-dir'.
+(defcustom quick-sdcv-only-data-dir t
+  "Only use the dictionaries in data-dir `quick-sdcv-dictionary-data-dir'.
 Do not search in user and system directories"
   :type 'boolean
-  :group 'easysdcv)
+  :group 'quick-sdcv)
 
-(defcustom easysdcv-exact-search nil
+(defcustom quick-sdcv-exact-search nil
   "Do not fuzzy-search for similar words, only return exact matches."
   :type 'boolean
-  :group 'easysdcv)
+  :group 'quick-sdcv)
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;; Variable ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-(defvar easysdcv-current-translate-object nil
+(defvar quick-sdcv-current-translate-object nil
   "The search object.")
 
-(defvar easysdcv-fail-notify-string
+(defvar quick-sdcv-fail-notify-string
   "If there is no explanation available, consider searching with additional
   dictionaries. User notification message for a failed search.")
 
-(defvar easysdcv-mode-font-lock-keywords
+(defvar quick-sdcv-mode-font-lock-keywords
   '(;; Dictionary name
     ("^-->\\(.*\\)\n-" . (1 font-lock-type-face))
     ;; Search word
@@ -123,19 +123,19 @@ Do not search in user and system directories"
     ;; Phonetic symbol
     ("^/\\([^>]*\\)/$" . (1 font-lock-string-face))
     ("^\\[\\([^]]*\\)\\]$" . (1 font-lock-string-face)))
-  "Expressions to highlight in `easysdcv-mode'.")
+  "Expressions to highlight in `quick-sdcv-mode'.")
 
 ;; Optionally, you might want to define the mode itself here.
-(defvar easysdcv-mode-map
+(defvar quick-sdcv-mode-map
   (let ((map (make-sparse-keymap)))
     map))
 
-(define-derived-mode easysdcv-mode nil "sdcv"
+(define-derived-mode quick-sdcv-mode nil "sdcv"
   "Major mode to look up word through sdcv.
-\\{easysdcv-mode-map}
+\\{quick-sdcv-mode-map}
 
-Enabling this mode runs the normal hook `easysdcv-mode-hook`."
-  (setq font-lock-defaults '(easysdcv-mode-font-lock-keywords t))
+Enabling this mode runs the normal hook `quick-sdcv-mode-hook`."
+  (setq font-lock-defaults '(quick-sdcv-mode-font-lock-keywords t))
   (setq buffer-read-only t)
   (set (make-local-variable 'outline-regexp) "^-->.*\n-->")
   (set (make-local-variable 'outline-level) #'(lambda()
@@ -145,91 +145,91 @@ Enabling this mode runs the normal hook `easysdcv-mode-hook`."
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;; Interactive Functions ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 ;;;###autoload
-(defun easysdcv-search-pointer ()
+(defun quick-sdcv-search-pointer ()
   "Retrieve the word under the cursor and display its definition.
 It displays the result in another buffer."
   (interactive)
-  (easysdcv--search-detail (easysdcv--get-region-or-word)))
+  (quick-sdcv--search-detail (quick-sdcv--get-region-or-word)))
 
 ;;;###autoload
-(defun easysdcv-search-input (&optional word)
+(defun quick-sdcv-search-input (&optional word)
   "Translate the specified input WORD and display the results in another buffer.
 
 If WORD is not provided, the function prompts the user to enter a word.
-The details will be shown in the `easysdcv-buffer-name' buffer."
+The details will be shown in the `quick-sdcv-buffer-name' buffer."
   (interactive)
-  (easysdcv--search-detail (or word (easysdcv--prompt-input))))
+  (quick-sdcv--search-detail (or word (quick-sdcv--prompt-input))))
 
 ;;;###autoload
-(defun easysdcv-list-dictionaries ()
+(defun quick-sdcv-list-dictionaries ()
   "List all available dictionaries in a separate buffer."
   (interactive)
-  (let ((dicts (easysdcv--get-list-dicts)))
-    (with-output-to-temp-buffer easysdcv-buffer-name
+  (let ((dicts (quick-sdcv--get-list-dicts)))
+    (with-output-to-temp-buffer quick-sdcv-buffer-name
       (dolist (dict dicts)
         (princ (format "%s\n" dict))))))
 
-(defun easysdcv-check ()
+(defun quick-sdcv-check ()
   "Check for missing StarDict dictionaries."
   (interactive)
-  (let* ((dicts (easysdcv--get-list-dicts))
-         (missing-complete-dicts (easysdcv--get-missing-dicts easysdcv-dictionary-complete-list dicts)))
+  (let* ((dicts (quick-sdcv--get-list-dicts))
+         (missing-complete-dicts (quick-sdcv--get-missing-dicts quick-sdcv-dictionary-complete-list dicts)))
     (if (not missing-complete-dicts)
         (message "The dictionary's settings look correct, sdcv should work as expected.")
       (dolist (dict missing-complete-dicts)
-        (message "easysdcv-dictionary-complete-list: dictionary '%s' does not exist, remove it or download the corresponding dictionary file to %s"
-                 dict easysdcv-dictionary-data-dir)))))
+        (message "quick-sdcv-dictionary-complete-list: dictionary '%s' does not exist, remove it or download the corresponding dictionary file to %s"
+                 dict quick-sdcv-dictionary-data-dir)))))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;; Utilities Functions ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-(defun easysdcv--call-process (&rest arguments)
-  "Call `easysdcv-program' with ARGUMENTS.
+(defun quick-sdcv--call-process (&rest arguments)
+  "Call `quick-sdcv-program' with ARGUMENTS.
 Result is parsed as json."
-  (unless (executable-find easysdcv-program)
+  (unless (executable-find quick-sdcv-program)
     (error (concat "The program '%s' is not found. Please ensure it is "
                    "installed and the path is correctly set "
-                   "in `easysdcv-program`.")
-           easysdcv-program))
+                   "in `quick-sdcv-program`.")
+           quick-sdcv-program))
   (with-temp-buffer
     (save-excursion
-      (let ((exit-code (apply #'call-process easysdcv-program nil t nil
+      (let ((exit-code (apply #'call-process quick-sdcv-program nil t nil
                               (append (list "--non-interactive" "--json-output")
-                                      (when easysdcv-exact-search
+                                      (when quick-sdcv-exact-search
                                         (list "--exact-search"))
-                                      (when easysdcv-only-data-dir
+                                      (when quick-sdcv-only-data-dir
                                         (list "--only-data-dir"))
-                                      (when easysdcv-dictionary-data-dir
-                                        (list "--data-dir" easysdcv-dictionary-data-dir))
+                                      (when quick-sdcv-dictionary-data-dir
+                                        (list "--data-dir" quick-sdcv-dictionary-data-dir))
                                       arguments))))
         (if (not (zerop exit-code))
-            (error "Failed to call %s: exit code %d" easysdcv-program
+            (error "Failed to call %s: exit code %d" quick-sdcv-program
                    exit-code))))
     (ignore-errors (json-read))))
 
-(defun easysdcv--get-list-dicts ()
+(defun quick-sdcv--get-list-dicts ()
   "List dictionaries present in SDCV."
   (mapcar (lambda (dict) (cdr (assq 'name dict)))
-          (easysdcv--call-process "--list-dicts")))
+          (quick-sdcv--call-process "--list-dicts")))
 
-(defun easysdcv--get-missing-dicts (list &optional dicts)
+(defun quick-sdcv--get-missing-dicts (list &optional dicts)
   "List missing LIST dictionaries in DICTS.
 If DICTS is nil, compute present dictionaries with
-`easysdcv--get-list-dicts'."
-  (let ((dicts (or dicts (easysdcv--get-list-dicts))))
+`quick-sdcv--get-list-dicts'."
+  (let ((dicts (or dicts (quick-sdcv--get-list-dicts))))
     (cl-set-difference list dicts :test #'string=)))
 
-(defun easysdcv--search-detail (&optional word)
-  "Search WORD in `easysdcv-dictionary-complete-list'.
+(defun quick-sdcv--search-detail (&optional word)
+  "Search WORD in `quick-sdcv-dictionary-complete-list'.
 The result will be displayed in buffer named with
-`easysdcv-buffer-name' in `easysdcv-mode'."
+`quick-sdcv-buffer-name' in `quick-sdcv-mode'."
   (when word
     (message "Searching...")
-    (with-current-buffer (get-buffer-create easysdcv-buffer-name)
+    (with-current-buffer (get-buffer-create quick-sdcv-buffer-name)
       (setq buffer-read-only nil)
       (erase-buffer)
-      (setq easysdcv-current-translate-object word)
-      (insert (easysdcv--search-with-dictionary word easysdcv-dictionary-complete-list))
-      (easysdcv--goto-sdcv)
+      (setq quick-sdcv-current-translate-object word)
+      (insert (quick-sdcv--search-with-dictionary word quick-sdcv-dictionary-complete-list))
+      (quick-sdcv--goto-sdcv)
       ;; Re-initialize buffer. Hide all entry but the first one and goto the
       ;; beginning of the buffer.
       (ignore-errors
@@ -237,21 +237,21 @@ The result will be displayed in buffer named with
         (goto-char (point-min))
         (outline-next-heading)
         (outline-show-all)
-        (message "Finished searching `%s'." easysdcv-current-translate-object)))))
+        (message "Finished searching `%s'." quick-sdcv-current-translate-object)))))
 
-(defun easysdcv--search-with-dictionary (word dictionary-list)
+(defun quick-sdcv--search-with-dictionary (word dictionary-list)
   "Search some WORD with DICTIONARY-LIST.
 Argument DICTIONARY-LIST the word that needs to be transformed."
-  (let* ((word (or word (easysdcv--get-region-or-word)))
-         (translate-result (easysdcv--translate-result word dictionary-list)))
+  (let* ((word (or word (quick-sdcv--get-region-or-word)))
+         (translate-result (quick-sdcv--translate-result word dictionary-list)))
 
-    (when (and (string= easysdcv-fail-notify-string translate-result)
+    (when (and (string= quick-sdcv-fail-notify-string translate-result)
                (setq word (thing-at-point 'word t)))
-      (setq translate-result (easysdcv--translate-result word dictionary-list)))
+      (setq translate-result (quick-sdcv--translate-result word dictionary-list)))
 
     translate-result))
 
-(defun easysdcv--translate-result (word dictionary-list)
+(defun quick-sdcv--translate-result (word dictionary-list)
   "Call sdcv to search WORD in DICTIONARY-LIST.
 Return filtered string of results."
   (let* ((arguments (cons word (mapcan (lambda (d) (list "-u" d)) dictionary-list)))
@@ -259,15 +259,15 @@ Return filtered string of results."
                   (lambda (result)
                     (let-alist result
                       (format "-->%s\n-->%s\n%s\n\n" .dict .word .definition)))
-                  (apply #'easysdcv--call-process arguments)
+                  (apply #'quick-sdcv--call-process arguments)
                   "")))
     (if (string-empty-p result)
-        easysdcv-fail-notify-string
+        quick-sdcv-fail-notify-string
       result)))
 
-(defun easysdcv--goto-sdcv ()
+(defun quick-sdcv--goto-sdcv ()
   "Switch to sdcv buffer in other window."
-  (let* ((buffer (easysdcv--get-buffer))
+  (let* ((buffer (quick-sdcv--get-buffer))
          (window (get-buffer-window buffer)))
     (if (null window)
         ;; Use display-buffer because it follows display-buffer-alist
@@ -276,21 +276,21 @@ Return filtered string of results."
             (select-window win)))
       (select-window window))))
 
-(defun easysdcv--get-buffer ()
+(defun quick-sdcv--get-buffer ()
   "Get the sdcv buffer.  Create one if there's none."
-  (let ((buffer (get-buffer-create easysdcv-buffer-name)))
+  (let ((buffer (get-buffer-create quick-sdcv-buffer-name)))
     (with-current-buffer buffer
-      (unless (eq major-mode 'easysdcv-mode)
-        (easysdcv-mode)))
+      (unless (eq major-mode 'quick-sdcv-mode)
+        (quick-sdcv-mode)))
     buffer))
 
-(defun easysdcv--prompt-input ()
+(defun quick-sdcv--prompt-input ()
   "Prompt input for translation."
-  (let* ((word (easysdcv--get-region-or-word))
+  (let* ((word (quick-sdcv--get-region-or-word))
          (default (if word (format " (default %s)" word) "")))
     (read-string (format "Word%s: " default) nil nil word)))
 
-(defun easysdcv--get-region-or-word ()
+(defun quick-sdcv--get-region-or-word ()
   "Return region or word around point.
 If `mark-active' on, return region string.
 Otherwise return word around point."
@@ -298,6 +298,6 @@ Otherwise return word around point."
       (buffer-substring-no-properties (region-beginning) (region-end))
     (thing-at-point 'word t)))
 
-(provide 'easysdcv)
+(provide 'quick-sdcv)
 
-;;; easysdcv.el ends here
+;;; quick-sdcv.el ends here
