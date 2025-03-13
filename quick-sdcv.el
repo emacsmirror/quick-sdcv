f94099124e4df92b44086c46d59e286f21bfafa8
James Cherti <60946298+jamescherti@users.noreply.github.com>
2025-03-13 13:03:00 -0400

    Release: 1.0.1

--- quick-sdcv.el
+++ quick-sdcv.el
@@ -8,7 +8,7 @@
 ;; Package-Requires: ((emacs "25.1"))
 ;; Maintainer: James Cherti
 ;; Original Author: Andy Stewart
-;; Version: 1.0.0
+;; Version: 1.0.1
 ;; URL: https://github.com/jamescherti/quick-sdcv.el
 ;; Keywords: docs, startdict, sdcv
 
